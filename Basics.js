50d5350d283ba341585892aaa9c929980abaa04b
Nikhil Kholiya <nikhilkholiya08@gmail.com>
2025-12-20 17:53:45 +0530

    Typescript Basics-1-clear-move to practice exercises

--- Basics.js
+++ Basics.js
@@ -1,3 +1,12 @@
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
 // *add this folder in github repo and practice the typescript basics
 console.log('Hello, Typescript!');
 //*Basic types in TypeScript
@@ -28,3 +37,59 @@ var employee = {
     salary: 500000
 };
 console.log('Employee Details:', employee);
+var car = {
+    name: 'BMW',
+    modelNumber: 'BMWX5',
+    color: 'Black',
+    price: 60000000,
+    isAvailable: true
+};
+console.log('Car interface details:', car);
+//*Function with Type Annoations
+console.log('--Function with Type Annoations---');
+console.log(' Function1-Create a function add two numbers with type annoations');
+function add(x, y) {
+    return x + y;
+}
+var sum = add(1000, 1000);
+console.log("Sum:".concat(sum));
+console.log('Function2-Create a function multiply two number with type annoatitons');
+function multiply(a, b) {
+    return a * b;
+}
+var multiplication = multiply(10, 20);
+console.log("Multiplication:".concat(multiplication));
+console.log('Function3-create a function to greet a user with optional parameter');
+function greet(name, greeting) {
+    return "Hello mr".concat(name, ",").concat(greeting ? greeting : 'Welcome to TypeScript!');
+}
+var greetMessage1 = greet('Nikhil', 'Good Morning!');
+console.log(greetMessage1);
+var greetMessage2 = greet('Alex');
+console.log(greetMessage2);
+//*Enums in Typescript
+console.log('---Enums in Typescript---');
+var Direction;
+(function (Direction) {
+    Direction[Direction["North"] = 0] = "North";
+    Direction[Direction["South"] = 1] = "South";
+    Direction[Direction["East"] = 2] = "East";
+    Direction[Direction["West"] = 3] = "West";
+})(Direction || (Direction = {}));
+var myDirection = Direction.South;
+console.log("My Direction is".concat(myDirection));
+//*Generics in TypeScript
+console.log('---Generics in TypeScript---');
+console.log('Create a generic funtion to retrun any type of array');
+function getArray(items) {
+    return new (Array.bind.apply(Array, __spreadArray([void 0], items, false)))();
+}
+var numberArray = getArray([1, 2, 3, 4]);
+console.log('Number Array:', numberArray);
+var strArray = getArray(['Nikhil', 'Alex', 'John']);
+console.log('String Array:', strArray);
+//*Type Assertions in TypeScript
+console.log('---Type Assertions in TypeScript---');
+var someValue = 'This is a string';
+var strLength = someValue.length;
+console.log("String Length:".concat(strLength));
--- Basics.ts
+++ Basics.ts
@@ -40,3 +40,75 @@ let employee: Employee = {
 }
 
 console.log('Employee Details:', employee);
+
+//*Interfaces in typescript
+interface Car{
+    name: string,
+    modelNumber: number|string,
+    color: string,
+    price: number,
+    isAvailable:boolean,
+}
+
+let car: Car = {
+    name: 'BMW',
+    modelNumber: 'BMWX5',
+    color: 'Black',
+    price: 60000000,
+    isAvailable:true
+}
+
+console.log('Car interface details:', car);
+
+//*Function with Type Annoations
+console.log('--Function with Type Annoations---');
+console.log(' Function1-Create a function add two numbers with type annoations');
+function add(x: number, y: number): number{
+    return x + y;
+}
+let sum: number = add(1000, 1000);
+console.log(`Sum:${sum}`)
+
+console.log('Function2-Create a function multiply two number with type annoatitons');
+function multiply(a:number,b:number): number{
+    return a * b;
+}
+let multiplication: number = multiply(10, 20);
+console.log(`Multiplication:${multiplication}`)
+
+console.log('Function3-create a function to greet a user with optional parameter');
+function greet(name: string, greeting?: string): string{
+    return `Hello mr${name},${greeting ? greeting : 'Welcome to TypeScript!'}`;
+}
+let greetMessage1: string = greet('Nikhil', 'Good Morning!');
+console.log(greetMessage1);
+let greetMessage2: string = greet('Alex');
+console.log(greetMessage2);
+
+//*Enums in Typescript
+console.log('---Enums in Typescript---');
+enum Direction{
+    North,
+    South,
+    East,
+    West
+}
+let myDirection= Direction.South
+console.log(`My Direction is${myDirection}`)
+
+//*Generics in TypeScript
+console.log('---Generics in TypeScript---');
+console.log('Create a generic funtion to retrun any type of array');
+function getArray<T>(items: T[]): T[]{
+    return new Array(...items); 
+}
+let numberArray = getArray<number>([1, 2, 3, 4]);
+console.log('Number Array:',numberArray)
+let strArray=getArray<string>(['Nikhil','Alex','John']);
+console.log('String Array:', strArray);
+
+//*Type Assertions in TypeScript
+console.log('---Type Assertions in TypeScript---');
+let someValue: any = 'This is a string';
+let strLength: number = (someValue as string).length;
+console.log(`String Length:${strLength}`);
