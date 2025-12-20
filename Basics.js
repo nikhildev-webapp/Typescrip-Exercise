a5a43be92ed18645b76888ad0a5de1b4689fa744
Nikhil Kholiya <nikhilkholiya08@gmail.com>
2025-12-20 17:18:28 +0530

    Basics of typescript-inprogress

--- Basics.js
+++ Basics.js
@@ -1,2 +1,30 @@
 // *add this folder in github repo and practice the typescript basics
 console.log('Hello, Typescript!');
+//*Basic types in TypeScript
+console.log('---Basic types in Typescript---');
+var username = 'Nikhil';
+var age = 20;
+var isActive = true;
+console.log("Username:".concat(username, ",Age:").concat(age, ",Active:").concat(isActive));
+//*Infernce types
+console.log('---Inference types---');
+var city = 'New York';
+var population = 100000000;
+console.log(typeof city);
+console.log(typeof population);
+//*Union Types
+console.log('--Union Types---');
+var userId;
+userId = 'Alex123';
+console.log("UserId as a string:".concat(userId));
+userId = 12345;
+console.log("UserId as a number:".concat(userId));
+//*Type Aliases;
+console.log('---Type Aliases---');
+var employee = {
+    name: 'Nikhil',
+    age: 20,
+    position: 'Developer',
+    salary: 500000
+};
+console.log('Employee Details:', employee);
--- Basics.ts
+++ Basics.ts
@@ -1,2 +1,42 @@
 // *add this folder in github repo and practice the typescript basics
 console.log('Hello, Typescript!')
+
+//*Basic types in TypeScript
+console.log('---Basic types in Typescript---');
+let username: string = 'Nikhil';
+let age: number = 20;
+let isActive: boolean = true;
+console.log(`Username:${username},Age:${age},Active:${isActive}`);
+
+//*Infernce types
+console.log('---Inference types---');
+let city: string = 'New York';
+let population: number = 100000000;
+console.log(typeof city);
+console.log(typeof population);
+
+//*Union Types
+console.log('--Union Types---');
+let userId: string | number;
+userId = 'Alex123';
+console.log(`UserId as a string:${userId}`);
+userId = 12345;
+console.log(`UserId as a number:${userId}`);
+
+//*Type Aliases;
+console.log('---Type Aliases---');
+type Employee = {
+    name: string,
+    age: number,
+    position: string,
+    salary: number,
+}
+
+let employee: Employee = {
+    name: 'Nikhil',
+    age: 20,
+    position: 'Developer',
+    salary:500000
+}
+
+console.log('Employee Details:', employee);
