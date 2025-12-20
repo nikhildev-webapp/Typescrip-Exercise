ec65cef832c4460c703c345609d05ef6cf35cdc7
Nikhil Kholiya <nikhilkholiya08@gmail.com>
2025-12-20 18:35:00 +0530

    LevelOne-Exercise complete

--- LevelOne.js
+++ LevelOne.js
@@ -1,13 +1,11 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 //*Level:Basic Types & Varibales in TypeScript
 //*Exercise 1
 console.log('Exercise1:Declare variable with correct types');
-var name = 'Nikhil';
+var Name = 'Nikhil';
 var age = 20;
 var isStudent = true;
-console.log("Name: ".concat(name, ", Age: ").concat(age, ", Is Student: ").concat(isStudent));
-console.log('Types of variables:', typeof name, typeof age, typeof isStudent);
+console.log("Name: ".concat(Name, ", Age: ").concat(age, ", Is Student: ").concat(isStudent));
+console.log('Types of variables:', typeof Name, typeof age, typeof isStudent);
 //*Exercise 2
 console.log('Exercise2: create a varibale that store either a string or number, Assign both values one by one');
 var userId;
@@ -15,3 +13,22 @@ userId = 'User1';
 console.log("UserId as string: ".concat(userId));
 userId = 101;
 console.log("UserId as number: ".concat(userId));
+//Exercise 3
+console.log('Exercise3:Create a array that stores only numbers, Add 5 number to it');
+var numberArray = [1, 2, 3, 4];
+console.log('Initial number array:', numberArray);
+numberArray.push(5);
+console.log('Number array after adding number', numberArray);
+//Exercise 4
+console.log('Exercise4:create an array that can store string or number values');
+var mixedArray = ['Nikhil', 25, 'Developer', 500000];
+console.log('Mixed array:', mixedArray);
+//*Exercise 5
+console.log('Exercise5:Use type interface, Declare a variable and assign a value,');
+var Student = {
+    name: 'Nikhil',
+    age: 20,
+    Class: 'Final Year',
+    ispresent: true,
+};
+console.log('Student Details:', Student);
--- LevelOne.ts
+++ LevelOne.ts
@@ -1,12 +1,12 @@
-export {}; //for module scope for avoiding global scope issues
+
 //*Level:Basic Types & Varibales in TypeScript
 //*Exercise 1
 console.log('Exercise1:Declare variable with correct types');
-let name: string = 'Nikhil';
+let Name: string = 'Nikhil';
 let age: number = 20;
 let isStudent: boolean = true;
-console.log(`Name: ${name}, Age: ${age}, Is Student: ${isStudent}`);
-console.log('Types of variables:', typeof name, typeof age, typeof isStudent);
+console.log(`Name: ${Name}, Age: ${age}, Is Student: ${isStudent}`);
+console.log('Types of variables:', typeof Name, typeof age, typeof isStudent);
 
 //*Exercise 2
 console.log('Exercise2: create a varibale that store either a string or number, Assign both values one by one');
@@ -15,3 +15,33 @@ userId = 'User1';
 console.log(`UserId as string: ${userId}`);
 userId = 101;
 console.log(`UserId as number: ${userId}`); 
+
+//Exercise 3
+console.log('Exercise3:Create a array that stores only numbers, Add 5 number to it');
+let numberArray: number[] = [1, 2, 3, 4];
+console.log('Initial number array:', numberArray);
+numberArray.push(5);
+console.log('Number array after adding number', numberArray);
+
+//Exercise 4
+console.log('Exercise4:create an array that can store string or number values');
+let mixedArray: (string | number)[] = ['Nikhil', 25, 'Developer', 500000];
+console.log('Mixed array:', mixedArray);
+
+//*Exercise 5
+console.log('Exercise5:Use type interface, Declare a variable and assign a value,');
+type Student = {
+    name: string,
+    age: number,
+    Class: string | number,
+    ispresent:boolean,
+}
+
+let Student: Student = {
+    name: 'Nikhil',
+    age: 20,
+    Class: 'Final Year',
+    ispresent:true,
+}
+
+console.log('Student Details:', Student);
