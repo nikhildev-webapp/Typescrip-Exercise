d3a0b450007d7034855d89907ef189641fee4f0c
Nikhil Kholiya <nikhilkholiya08@gmail.com>
2025-12-22 17:14:33 +0530

    LevelThree-exercise started

---
+++ LevelThree.js
@@ -0,0 +1,18 @@
+//*Level3:Type Aliases
+console.log("Level 3:Type Aliases");
+//*Exercise1:create a type alias for two user
+console.log('Exercise1:type alias for two user');
+var user1 = {
+    id: 'User101',
+    name: 'Nikhil',
+    email: 'nikhilkholiya@gmail.com',
+    isActive: true
+};
+console.log('User-1:', user1);
+var user2 = {
+    id: 'User102',
+    name: 'Harsh',
+    email: 'harsh@gmail.com',
+    isActive: false
+};
+console.log('User-2', user2);
---
+++ LevelThree.ts
@@ -0,0 +1,26 @@
+//*Level3:Type Aliases
+console.log("Level 3:Type Aliases");
+//*Exercise1:create a type alias for two user
+console.log('Exercise1:type alias for two user');
+type User = {
+    id: string,
+    name: string,
+    email: string;
+    isActive:boolean
+}
+
+let user1: User = {
+    id: 'User101',
+    name: 'Nikhil',
+    email: 'nikhilkholiya@gmail.com',
+    isActive:true  
+}
+console.log('User-1:', user1);
+
+let user2: User = {
+    id: 'User102',
+    name: 'Harsh',
+    email: 'harsh@gmail.com',
+    isActive:false
+}
+console.log('User-2', user2)
